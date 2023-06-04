f9f2652138fe557b8b9df18ad185a35e4de08c74
Ali Khancherli <a.khancherli2017@gmail.com>
2023-06-04 11:51:33 +0330

    jwt has setup in program.cs and configured (ApplicationDbContext will added later)

--- ToDo.App.Api/Program.cs
+++ ToDo.App.Api/Program.cs
@@ -1,3 +1,7 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using ToDo.App.Domain.Entities;
 using ToDo.App.Infrastructure.Options;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,6 +15,56 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthentication(opt =>
+{
+    opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+    opt.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+}).AddJwtBearer(opt =>
+{
+    var jwtOptions = builder.Configuration.GetSection(nameof(JwtConfig)).Get<JwtConfig>();
+
+    opt.RequireHttpsMetadata = false;
+    opt.SaveToken = true;
+
+    opt.TokenValidationParameters = new TokenValidationParameters()
+    {
+        RequireSignedTokens = true,
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtOptions.SecretKey)),
+        TokenDecryptionKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtOptions.EncryptKey)),
+        ValidateIssuer = true,
+        ValidIssuer = jwtOptions.Issuer,
+        ValidateAudience = true,
+        ValidAudience = jwtOptions.Audience
+    };
+
+    opt.Events = new JwtBearerEvents()
+    {
+        OnChallenge = ctx =>
+        {
+            if (ctx.AuthenticateFailure != null)
+                throw new UnauthorizedAccessException("مشکلی در احرازهویت شما رخ داده است");
+
+            throw new UnauthorizedAccessException("شما احرازهویت نشده اید");
+        }
+    };
+});
+
+
+builder.Services.AddIdentity<User, Role>(opt =>
+{
+    opt.SignIn.RequireConfirmedPhoneNumber = false;
+    opt.SignIn.RequireConfirmedEmail = false;
+    opt.SignIn.RequireConfirmedAccount = true;
+
+    opt.Password.RequireNonAlphanumeric = false;
+    opt.Password.RequireDigit = false;
+    opt.Password.RequireLowercase = false;
+    opt.Password.RequireUppercase = false;
+});//TODO : .AddEntityFrameworkStores<ApplicationDbContext>();
+
 
 var app = builder.Build();
 
